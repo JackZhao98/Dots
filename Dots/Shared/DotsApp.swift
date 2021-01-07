8c6a99c7ef76601f7a30aa53e1fa68e7753eb899
Jack Zhao <jack_zhao98@outlook.com>
2021-01-06 22:34:42 -0800

    Project file created'

---
+++ Dots/Shared/DotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotsApp.swift
+//  Shared
+//
+//  Created by Jack Zhao on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
